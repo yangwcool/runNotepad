3d97bd22f6307e9c30624c9811fdf2df8a7f9db9
yangwcool <35167776+yangwcool@users.noreply.github.com>
2022-02-21 13:27:56 +0800

    creat txt file every month
    - creat txt file every month, file name is note202202.txt;
    - use `start` to start notepad3.exe

--- runNotepad.bat
+++ runNotepad.bat
@@ -1,4 +1,7 @@
-d:
-cd \GreenPower\Notepad3
-::%~dp0代表当前bat文件所在目录
-Notepad3.exe  %~dp0\sticky_note3.txt
+@echo off
+
+rem %~dp0代表当前bat文件所在目录
+set month=%date:~0,4%
+set day=%date:~5,2%
+set filename=note%month%%day%.txt
+start "" "d:\GreenPower\Notepad3\Notepad3.exe" %~dp0%filename%
